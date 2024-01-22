6a4d65f25d81020c344746660c1368a62af86239
py <90938238g@gmail.com>
2024-01-23 01:55:10 +0800

    refactor: 单独抽离设置url查询参数的hook，重构useUrlQueryParam

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -5,10 +5,12 @@ import { useSearchParams, URLSearchParamsInit } from "react-router-dom"
 import { cleanObject } from ".";
 
 
-//从url中读取查询参数
+//从url中读取特定查询参数
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
 
-    const [searchParam, setSearchParam] = useSearchParams(); //注意这里的searchParam放在useMemo依赖项当中是没有问题的,不会造成多次渲染导致状态一直不变从而造成无限循环
+    const [searchParam] = useSearchParams(); //注意这里的searchParam放在useMemo依赖项当中是没有问题的,不会造成多次渲染导致状态一直不变从而造成无限循环
+
+    const setSearchParam = useSetUrlSearchParam()
     
     return [
          useMemo(
@@ -23,10 +25,22 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
              //eslint-disable-next-line react-hooks/exhaustive-deps
              [searchParam]
         )
-        , (params: Partial<{[key in K]: unknown}>) => {
-            const  o = cleanObject({...Object.fromEntries(searchParam), ...params}) as URLSearchParamsInit
-            return setSearchParam(o)
+        , 
+        (params: {[key in string]: unknown}) => {
+            return setSearchParam(params)
         }
     ] as const //元祖
     
+}
+
+
+//单独抽离出hook，方便其设立url search param
+export const useSetUrlSearchParam = () => {
+
+    const [searchParam, setSearchParam] = useSearchParams(); //注意这里的searchParam放在useMemo依赖项当中是没有问题的,不会造成多次渲染导致状态一直不变从而造成无限循环
+
+    return (params: {[key in string]: unknown}) => {
+        const  o = cleanObject({...Object.fromEntries(searchParam), ...params}) as URLSearchParamsInit
+        return setSearchParam(o)
+    }
 }
