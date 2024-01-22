63c6ec09b7d2553beeaf83726c17ed9a81b2ca6d
py <90938238g@gmail.com>
2024-01-23 01:58:45 +0800

    feat: 添加获取queryKey hook，重构获取modal状态的hook

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,5 +1,6 @@
-import { useUrlQueryParam } from "@/utils/url"
-import { useMemo } from "react"
+import { useProject } from "@/utils/project"
+import { useSetUrlSearchParam, useUrlQueryParam } from "@/utils/url"
+import {  useMemo } from "react"
 
 export const useProjectsSearchParams = ()  => {
     
@@ -20,25 +21,45 @@ export const useProjectsSearchParams = ()  => {
 //利用路由参数管理模态框的关闭和打开
 export const useProjectModal = () => {
 
-    //理由url参数来保存状态，起到了全局状态管理器的功能
-    const [{ projectCreate }, selectProjectModalOpen] = useUrlQueryParam(['projectCreate', ])
+    //理由url查询参数来保存状态，起到了全局状态管理器的功能
+    //判断是否处在创建项目状态
+    const [{ projectCreate } ] = useUrlQueryParam(['projectCreate'])
+    //用来判断是否在编辑状态
+    const [{ editingProjectId } ] = useUrlQueryParam(['editingProjectId'])
+
+    const setUrlSearchParm = useSetUrlSearchParam();
+
+    //根据id找出要被更新的project
+    const {data: editingProject, isLoading} = useProject(Number(editingProjectId))
+
+    const startEdit = (id: number) => {
+        setUrlSearchParm({ editingProjectId: id })
+    }
     
-    const open = () => selectProjectModalOpen({projectCreate: true});
-    const close = () => selectProjectModalOpen({projectCreate: ''});
+    const open = () => {
+        setUrlSearchParm({projectCreate: true});
+    }
     
-    //换回tuple
-    // return  [
-    //     projectCreate === 'true', //从url中读取数据，直接是字符串true
-    //     open,
-    //     close
-    // ] as const 
+    const close = () => {
+        setUrlSearchParm({
+            projectCreate: '',
+            editingProjectId: ''
+        })
+    }
 
     return {
         open,
         close,
-        projectModalOpen: projectCreate === 'true'
+        projectModalOpen: projectCreate === 'true' || !!editingProject,
+        startEdit,
+        editingProject,
+        isLoading
     }
+}
 
-    
 
+//获取projects的queryKey
+export const useProjectQueryKey = () => {
+    const [searchParam] = useProjectsSearchParams()
+    return ['projects', searchParam]
 }
